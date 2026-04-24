138efc7f8b82ca440d6dae8f921e9af392b0188c
0907 <3292461768@qq.com>
2026-04-24 09:10:09 +0800

    fix: resolve typescript type errors for Navbar props in docs and tech pages

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -12,8 +12,8 @@ export default function Docs() {
         connectWallet={() => {}}
         loginWithGitHub={() => {}}
         loginWithGoogle={() => {}}
-        loginWithEmail={async () => {}}
-        signupWithEmail={async () => {}}
+        loginWithEmail={async (e, p) => {}}
+        signupWithEmail={async (e, p) => {}}
         logout={() => {}}
       />
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
